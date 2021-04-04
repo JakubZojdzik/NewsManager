fa8a3fff20b8404ec11cfd63f5f391012b15c383
Jakub <jakub.zojdzik@gmail.com>
2021-04-04 20:28:14 +0200

    New source of info added

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
-page_url = "https://www.polsatnews.pl/"
 userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
 headers = {'User-Agent': userAgent}
+
+page_url = "https://www.polsatnews.pl/"
 page = requests.get(page_url, headers=headers)
-effect = BeautifulSoup(page.content, 'html.parser')
-h1 = effect.find("h1")
-h2 = effect.find_all("h2", {"class": "news__title"})
-print(h1.get_text())
+effect = bs(page.content, 'html.parser')
+main1 = effect.find("h1", {"class": "news__title"}).get_text()
+commons = effect.find_all("h2", {"class": "news__title"})
+
+
+page_url = "https://tvn24.pl/polska/"
+page = requests.get(page_url, headers=headers)
+effect = bs(page.content, "html.parser")
+main2 = effect.find("h2", {"class": "heading article-title"}).get_text()
+
+
+print("BREAKING NEWS:\nPolsat News:\t", main1, "\nTVN24:\t\t\t", main2)
 print()
-i = 1
-for h in h2:
-    if i > 10:
-        break
-    print(h.get_text())
-    i += 1
+print("Common news:")
+for i in range(3):
+    print(commons[i].get_text())
